2692337b7b5afe0c970451bee3aad395b259b03c
davemflick <davemflick@yahoo.com>
2017-05-27 10:42:31 -0400

    Changed directions styling, added header

--- app/components/Directions.js
+++ app/components/Directions.js
@@ -4,6 +4,7 @@ export default class Directions extends React.Component{
 	render(){
 		return(
 			<div className="directions">
+				<h2> Instructions </h2>
 				<ul>
 					<li className="dirItem">Traverse all four levels and protect your yard!</li>
 					<li className="dirItem">Collect 'Toys' to use as weapons.</li>
@@ -12,6 +13,8 @@ export default class Directions extends React.Component{
 					<li className="dirItem">Enemies are the cats and the evil neighbor kid.</li>
 					<li className="dirItem">Collect 'Food Bowls' to increase health.</li>
 					<li className="dirItem">The higher your Character Level, the more damage you cause.</li>
+					<li className="dirItem">Defeat the evil neighbor kid to win the game.</li>
+					<li className="dirItem">Now, go and protect your yard!</li>
 				</ul>
 			</div>
 		)
